57a149a6d59c5007a47307d82644e3c0cc1556e9
Benjamin Sproule <benjamin@benjaminsproule.com>
2024-01-07 00:10:18 +0000

    Try and fix issues

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use std::time::{Duration, SystemTime};
 
 use drive3::DriveHub;
 use hyper::Client;
-use hyper::net::HttpsConnector;
-use hyper_rustls::TlsClient;
+use hyper::client::HttpConnector;
+use hyper_rustls::HttpsConnector;
 use log::{debug, error, LevelFilter, SetLoggerError};
 use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
@@ -19,7 +19,8 @@ use log4rs::config::{Appender, Config, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
 use log4rs::filter::threshold::ThresholdFilter;
 use log4rs::Handle;
-use oauth2::{ApplicationSecret, Authenticator, DefaultAuthenticatorDelegate, DiskTokenStorage};
+use oauth2::authenticator::Authenticator;
+use oauth2::{ApplicationSecret, InstalledFlowAuthenticator, InstalledFlowReturnMethod};
 use rusqlite::Connection;
 
 use crate::drive::{Drive, FileWrapper};
@@ -27,20 +28,21 @@ use crate::drive::{Drive, FileWrapper};
 mod drive;
 mod dbcontext;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let _handle = configure_logging().unwrap();
     let connection = get_db_connection();
-    let hub = DriveHub::new(get_client(), get_authenticator());
+    let hub = DriveHub::new(get_client(), get_authenticator().await);
 
     let drive = Drive::new(hub, connection);
-    drive.init();
+    drive.init().await;
 
     loop {
-        drive.store_fetched_files()?;
+        drive.store_fetched_files().await?;
         let existing_file_wrappers = drive.get_all_files(true).unwrap();
         debug!("Retrieved {} files", existing_file_wrappers.len());
         for file_wrapper in &existing_file_wrappers {
-            handle_existing_file(&drive, file_wrapper)
+            handle_existing_file(&drive, file_wrapper).await
         }
         let local_files: Vec<FileWrapper> = drive.get_local_files().unwrap();
         for file_wrapper in &local_files {
@@ -52,7 +54,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                     continue;
                 }
                 debug!("Upload {} to Google Drive for the first time", file_wrapper.path.display());
-                let result = drive.upload_file(file_wrapper);
+                let result = drive.upload_file(file_wrapper).await;
                 if result.is_err() {
                     error!("Error occurred whilst uploading {} to Google Drive for the first time. {}", file_wrapper.path.display(), result.unwrap_err())
                 }
@@ -62,13 +64,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 }
 
-fn handle_existing_file(drive: &Drive, file_wrapper: &FileWrapper) {
+async fn handle_existing_file(drive: &Drive, file_wrapper: &FileWrapper) {
     if file_wrapper.directory || file_wrapper.trashed {
         return;
     }
     if !file_wrapper.path.exists() {
         debug!("Creating file {} for the first time", file_wrapper.path.display());
-        let created = drive.create_file(file_wrapper);
+        let created = drive.create_file(file_wrapper).await;
         if created.is_err() {
             error!("Unable to create file {}.", file_wrapper.path.display())
         }
@@ -77,19 +79,19 @@ fn handle_existing_file(drive: &Drive, file_wrapper: &FileWrapper) {
         let remote_modified_time = file_wrapper.last_accessed.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
         if remote_modified_time == 0 {
             debug!("Remote modified time wasn't updated properly for file {} when it was created", file_wrapper.path.display());
-            let created = drive.create_file(file_wrapper);
+            let created = drive.create_file(file_wrapper).await;
             if created.is_err() {
                 error!("Unable to create file {}.", file_wrapper.path.display())
             }
         } else if local_modified_time > remote_modified_time {
             debug!("File {} has changed locally since last sync", file_wrapper.path.display());
-            let uploaded = drive.upload_file(file_wrapper);
+            let uploaded = drive.upload_file(file_wrapper).await;
             if uploaded.is_err() {
                 error!("Unable to create file {}.", file_wrapper.path.display())
             }
         } else if local_modified_time < remote_modified_time {
             debug!("File {} has changed on remote since last sync", file_wrapper.path.display());
-            let created = drive.create_file(file_wrapper);
+            let created = drive.create_file(file_wrapper).await;
             if created.is_err() {
                 error!("Unable to create file {}.", file_wrapper.path.display())
             }
@@ -122,22 +124,14 @@ fn configure_logging() -> Result<Handle, SetLoggerError> {
     log4rs::init_config(config)
 }
 
-fn get_client() -> Client {
-    Client::with_connector(HttpsConnector::new(TlsClient::new()))
+fn get_client() -> Client<HttpsConnector<HttpConnector>> {
+    Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().expect("msg").https_or_http().enable_http1().build())
 }
 
-fn get_authenticator() -> Authenticator<DefaultAuthenticatorDelegate, DiskTokenStorage, Client> {
-    let secret_json = include_str!("../secret.json").to_owned();
-    let secret: ApplicationSecret = yup_oauth2::parse_application_secret(&secret_json).expect("secret.json");
+async fn get_authenticator() -> Authenticator<drive3::hyper_rustls::HttpsConnector<HttpConnector>> {
+    let secret: ApplicationSecret = yup_oauth2::read_application_secret("../secret.json").await.expect("secret.json");
     let token_file = &get_base_data_path().join("temp-key").to_str().unwrap().to_owned();
-    let token_storage = DiskTokenStorage::new(&token_file).unwrap();
-    Authenticator::new(
-        &secret,
-        DefaultAuthenticatorDelegate,
-        get_client(),
-        token_storage,
-        Option::from(yup_oauth2::FlowType::InstalledInteractive),
-    )
+    InstalledFlowAuthenticator::builder(secret, InstalledFlowReturnMethod::Interactive).persist_tokens_to_disk(token_file).build().await.unwrap()
 }
 
 fn get_db_connection() -> Connection {
